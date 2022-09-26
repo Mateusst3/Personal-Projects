2908d7f1a9ce94c9b5cb6a27f8a912abea94f915
Mateus <mtex613@gmail.com>
2022-09-26 15:31:52 -0300

    Adicionando projeto maven

---
+++ fullproject/src/test/java/br/com/mateusteixeira/fullproject/FullprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusteixeira.fullproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
