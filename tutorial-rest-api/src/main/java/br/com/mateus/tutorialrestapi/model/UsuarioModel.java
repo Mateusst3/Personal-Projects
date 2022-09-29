3ec864db9ea6ee10b270e588c47ccf9bc496ac2b
Mateus <mtex613@gmail.com>
2022-09-29 15:46:52 -0300

    Adicionando primeiro entrypoint

---
+++ tutorial-rest-api/src/main/java/br/com/mateus/tutorialrestapi/model/UsuarioModel.java
@@ -0,0 +1,52 @@
+package br.com.mateus.tutorialrestapi.model;
+
+import javax.persistence.*;
+
+@Entity(name = "usuario")
+public class UsuarioModel {
+    @Id
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    public String nome;
+
+    @Column(nullable = false, length = 10)
+    public String login;
+
+    @Column(nullable = false, length = 10)
+    public String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
