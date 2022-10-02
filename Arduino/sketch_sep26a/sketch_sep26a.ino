179f0c5244cc811dac887e8ca0755114069cb27b
Mateus <mtex613@gmail.com>
2022-10-02 14:34:35 -0300

    Adicionando codigo arduino

--- Arduino/sketch_sep26a/sketch_sep26a.ino
+++ Arduino/sketch_sep26a/sketch_sep26a.ino
@@ -1,42 +1,68 @@
-int led = 14; // set the "led" variable as 14 - D5 ESP8266
+int buzzer = 14; // set the "buzzer" variable as 14 - D5 ESP8266
 int sonar_echo = 13; // set the "sonar_echo" variable as 13 - D7 ESP8266
 int sonar_trigger = 12; // set the "sonar_trigger" variable as 12 - D6 ESP8266
 
-//define sound velocity in cm/uS
-#define SOUND_VELOCITY 0.034
-#define CM_TO_INCH 0.393701
-
-long duration;
-float distance_cm;
-float distance_inch;
+const float som = 34300.0; // Velocidade do som em cm/s
+const float limite1 = 30.0; // Limites minimo para o buzzer ativar
+const float limite2 = 20.0;
+const float limite3 = 10.0;
+const float limite4 = 5.0;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(led, OUTPUT);   // designate port 13 as output
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);   // designate port 13 as output
   pinMode(sonar_trigger, OUTPUT);
   pinMode(sonar_echo, INPUT);
 }
  
 void loop() {
-  digitalWrite(led, LOW);
+  inicializarTrigger();
+  float distancia = calcularDistancia ();
+  if (distancia < limite1){
+  // Dispara os alertas
+    alertas(distancia);
+  }
+}
+
+void alertas (float distancia){
+  // Se a distância estiver entre o limite1 e o limite2
+  if (distancia < limite1 && distancia >= limite2){
+    tone(buzzer, 1500, 50);
+  }
+    // Se a distância estiver entre o limite2 e o limite3
+  else if (distancia < limite2 && distancia >= limite3){
+    tone(buzzer, 2000, 100);
+  }
+  
+  // Se a distância estiver entre o limite3 e o limite4
+  else if (distancia < limite3 && distancia >= limite4){
+    tone(buzzer, 2750, 150);
+  }
+  
+  // Se a distância for menor que o limite4
+  else if (distancia < limite4){
+    tone(buzzer, 3000, 200);
+    delay(2);
+    tone(buzzer, 3000, 200);
+    delay(2);
+    tone(buzzer, 3000, 200);
+  }
+}
+
+float calcularDistancia(){
+  unsigned long time = pulseIn(sonar_echo, HIGH);
+  float distancia = time* 0.000001 * som / 2.0;
+  Serial.print(distancia);
+  Serial.print("cm");
+  Serial.println();
+  delay(500);
+  return distancia;
+}
+
+void inicializarTrigger(){
   digitalWrite(sonar_trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(sonar_trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(sonar_trigger, LOW);
-
-  duration = pulseIn(sonar_echo, HIGH);
-
-    // Calculate the distance
-  distance_cm = duration * SOUND_VELOCITY/2;
-  
-  // Convert to inches
-  distance_inch = distance_cm * CM_TO_INCH;
-
-  
-  
-//  digitalWrite(led, HIGH);   // turn the led on
-//  delay(1000);               // wait for 1 second
-//  digitalWrite(led, LOW);    // turn the led off
-//  delay(1000);               // wait for a second
 }
