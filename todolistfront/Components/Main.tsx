262583456508f7c4369f1433d8a1d7e7237a1207
Mateus <mtex613@gmail.com>
2023-02-07 01:03:08 -0300

    Finalizando projeto

--- todolistfront/Components/Main.tsx
+++ todolistfront/Components/Main.tsx
@@ -6,98 +6,88 @@ import { get } from "../pages/api/apiHandler";
 import Note from "./Note/Note";
 import { StatusEnum } from "../public/interfaces/StatusEnum";
 
-
 export default function MainPage() {
-    const { data: session } = useSession()
-    const [update, setUpdate] = useState()
-    const [userNotes, setUserNotes] = useState<[] | undefined>(undefined)
-    const [charge, setCharge] = useState(5)
-    session?.user
-        ?
-        useEffect(() => {
-            get(`note/get/userEmail?userEmail=${session.user?.email}`).then((response) => setUserNotes(response))
+  const { data: session } = useSession();
+  const [update, setUpdate] = useState();
+  const [userNotes, setUserNotes] = useState<[] | undefined>(undefined);
+  const [charge, setCharge] = useState(5);
+  session?.user
+    ? useEffect(() => {
+        get(`note/get/userEmail?userEmail=${session.user?.email}`).then(
+          (response) => setUserNotes(response)
+        );
+      })
+    : useEffect(() => {});
 
-        })
-        :
-        useEffect(() => { })
-
-    return (
-        <>
-            <div className="w-screen h-screen bg-neutral-900 flex flex-row justify-center">
-                <div>
-                    <div className="pt-5 pr-10 flex justify-between">
-                        <h1 className="text-white pl-10 pt-4 text-3xl">
-                            To Do List
-                        </h1>
-                        <Login />
-                    </div>
-                    <div className="w-screen h-5/6 mt-6 rounded-3xl border-2 border-gray-50">
+  return (
+    <>
+      <div className='"w-screen h-screen bg-neutral-900 flex flex-row justify-center"'>
+        <div>
+          <div className="pt-5 pr-10 flex justify-between">
+            <h1 className="text-white pl-10 pt-4 text-3xl">To Do List</h1>
+            <Login />
+          </div>
+          <div className="w-screen h-5/6 mt-6 rounded-3xl border-2 border-gray-50">
+            {session?.user ? (
+              <>
+                <div className="p-3 flex flex-row h-full">
+                  {userNotes ? (
+                    <>
+                      <div className="w-1/5 h-full flex justify-center items-center border-r">
+                        <NewNoteButton qttNotes={userNotes.filter((value: any) => value.status != 'done').length}/>
+                      </div>
+                      <div className="flex flex-wrap w-full h-full gap-10 p-3 justify-center items-center">
                         {
-                            session?.user
-                                ?
-                                <>
-                                    <div className="p-3 flex flex-row h-full">
-                                        {
-                                            userNotes
-                                                ?
-                                                <>
-
-                                                    <div className="w-1/5 h-full flex justify-center items-center border-r">
-                                                        <NewNoteButton />
-                                                    </div>
-                                                    <div className="flex items-center text-white p-3">
-                                                        {
-                                                            charge > 5
-                                                                ?
-                                                                <button onClick={() => setCharge(charge - 5)} className='h-auto border rounded'>
-                                                                    Página anterior
-                                                                </button>
-                                                                :
-                                                                <></>
-                                                        }
-
-                                                    </div>
-                                                    <div className="flex flex-wrap gap-10 p-3 justify-center ">
-                                                        {
-                                                            userNotes.filter((element: any, index: number) => (element.status == StatusEnum.toDo)).map(
-                                                                (element: any) => {
-                                                                    return (
-                                                                        <Note id={element.id} title={element.title} content={element.content} status={element.status} setNotes={setUpdate} />
-                                                                    )
-                                                                }
-                                                            )
-                                                        }
-                                                    </div>
-                                                    {/* {
-                                                        userNotes.filter((element: any) => element.status == StatusEnum.toDo).length >= 5
-                                                            ?
-                                                            <div className="flex items-center text-white">
-                                                                <button onClick={() => setCharge(charge + 5)} className='h-auto border rounded'>
-                                                                    Próxima página
-                                                                </button>
-                                                            </div>
-                                                            :
-                                                            <></>
-                                                    } */}
-
-                                                </>
-                                                :
-                                                <></>
-                                        }
-                                    </div>
-                                </>
-                                :
-                                <>
-                                    <div className="flex flex-row justify-center items-center w-full h-full">
-                                        <h1 className="text-white pl-10 pt-4 text-3xl">
-                                            Você precisa fazer o login para acessar suas notas!
-                                        </h1>
-                                    </div>
-                                </>
-                        }
-                    </div>
+                        userNotes
+                            .filter(
+                              (element: any, index: number) =>
+                                element.status == StatusEnum.toDo
+                            ).length == 0
+                        ?
+                        <>
+                            <div className="h-full w-full flex justify-center items-center">
+                                <h1 className="text-white">
+                                    Sem notas registradas!
+                                </h1>
+                            </div>
+                        </>
+                        :
+                        
+                        userNotes
+                          .filter(
+                            (element: any, index: number) =>
+                              element.status == StatusEnum.toDo
+                          )
+                          .map((element: any) => {
+                            return (
+                              <Note
+                                id={element.id}
+                                title={element.title}
+                                content={element.content}
+                                status={element.status}
+                                setNotes={setUpdate}
+                              />
+                            );
+                          })}
+                      </div>
+                    </>
+                  ) : (
+                    <></>
+                  )}
                 </div>
-            </div>
-        </>
-    )
+              </>
+            ) : (
+              <>
+                <div className="flex flex-row justify-center items-center w-full h-full">
+                  <h1 className="text-white pl-10 pt-4 text-3xl">
+                    Você precisa fazer o login para acessar suas notas!
+                  </h1>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
